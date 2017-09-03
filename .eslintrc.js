db5a7892e8bb7f5ce24af9a33a2121fa6d1bd7e7
Michał Czyż <m.czyz@selleo.com>
2017-09-03 20:55:36 +0200

    tests and linters
    ember install ember-cli-mocha
    ember install ember-cli-template-lint
    yarn add eslint-plugin-ember -D

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,20 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:ember/recommended'],
   env: {
-    browser: true
+    browser: true,
   },
   rules: {
-  }
+    'no-console': [1, { allow: ['warn', 'error'] }],
+  },
+  'comma-dangle': [2, 'only-multiline'],
+  'space-before-function-paren': 'off',
+  'no-extra-semi': 2,
+  quotes: [2, 'single', 'avoid-escape'],
+  // follwing rules only for workshop (in production they should be defnied as errors)
+  'ember/named-functions-in-promises': 'warn',
+  'ember/alias-model-in-controller': 'warn',
 };
