4ada377ad68e8672f7f0d4639c79e8a3c0f6b80d
Michał Czyż <m.czyz@selleo.com>
2017-09-03 20:56:22 +0200

    conifgure @code

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,12 +10,12 @@ module.exports = {
   },
   rules: {
     'no-console': [1, { allow: ['warn', 'error'] }],
+    'comma-dangle': [2, 'only-multiline'],
+    'space-before-function-paren': 'off',
+    'no-extra-semi': 2,
+    quotes: [2, 'single', 'avoid-escape'],
+    // follwing rules only for workshop (in production they should be defnied as errors)
+    'ember/named-functions-in-promises': 'warn',
+    'ember/alias-model-in-controller': 'warn',
   },
-  'comma-dangle': [2, 'only-multiline'],
-  'space-before-function-paren': 'off',
-  'no-extra-semi': 2,
-  quotes: [2, 'single', 'avoid-escape'],
-  // follwing rules only for workshop (in production they should be defnied as errors)
-  'ember/named-functions-in-promises': 'warn',
-  'ember/alias-model-in-controller': 'warn',
 };
