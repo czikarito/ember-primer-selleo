e2723e694cc22cacb7ceea2fe22590295accc6de
Pawel Kuwik <p.kuwik@selleo.com>
2017-10-08 20:18:18 +0200

    Items route

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,31 +1,4 @@
 import Ember from 'ember';
 
-const { computed: { sort, uniq, mapBy }, computed } = Ember;
-
 export default Ember.Controller.extend({
-  nameSort: 'asc',
-  itemsSorted: sort('itemsFiltered', 'itemsSort'),
-  db: Ember.inject.service(),
-  model: computed(function(){
-    return this.get('db').all('items');
-  }),
-
-  itemsSort: computed('nameSort', function() {
-    return [`name:${this.get('nameSort')}`]
-  }),
-
-  itemsFiltered: computed('query', 'model.[]', function () {
-    let query = this.get('query');
-    let items = this.get('model');
-    let regexp = new RegExp(query, 'i');
-
-    return items.filter(item => regexp.test(item.name));
-  }),
-
-  actions: {
-    toggleSort() {
-      let newOrder = this.get('nameSort') == 'asc' ? 'desc' : 'asc';
-      this.set('nameSort', newOrder);
-    },
-  }
 });
