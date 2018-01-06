12dbbcce6b33ee68f9d255428b736ac90b760fad
Pawel Kuwik <pawel.kuwik@ac-project.net>
2018-01-06 12:44:23 +0100

    Router service

--- app/components/item-element.js
+++ app/components/item-element.js
@@ -1,11 +1,13 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { get } from '@ember/object';
 
 export default Component.extend({
-  // router: Ember.inject.service(),
+  router: service(),
   tagName: 'li',
   classNames: ['flex items-center lh-copy pa3 ph0-l bb b--black-10'],
 
-  // click() {
-  //   this.get('router').transitionTo('items.item', this.get('thing'));
-  // }
+  click() {
+    get(this, 'router').transitionTo('items.item', get(this, 'thing'));
+  }
 });
