070540f58b27a53d03ed15f6657459c403aa7eb4
Pawel Kuwik <pawel.kuwik@ac-project.net>
2018-01-06 11:59:21 +0100

    routing service

--- app/components/item-element.js
+++ app/components/item-element.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // router: Ember.inject.service(),
   tagName: 'li',
   classNames: ['flex items-center lh-copy pa3 ph0-l bb b--black-10'],
+
+  // click() {
+  //   this.get('router').transitionTo('items.item', this.get('thing'));
+  // }
 });
