48705d870ae66fb20f300f6caab161703ab5a5d2
Pawel Kuwik <pawel.kuwik@ac-project.net>
2018-01-07 12:54:15 +0100

    Components inheritance

---
+++ app/components/x-form/date.js
@@ -0,0 +1,8 @@
+import Input from '../x-form/input';
+import layout from 'track-items/templates/components/x-form/input';
+
+export default Input.extend({
+  layout,
+  inputType: 'date',
+  inputWidth: false
+});
--- app/components/x-form/input.js
+++ app/components/x-form/input.js
@@ -2,6 +2,18 @@ import Component from '@ember/component';
 import { computed, get } from '@ember/object';
 
 const Input = Component.extend({
+  inputType: 'string',
+  inputWidth: 100,
+  inputClassess: computed('inputWidth', function(){
+    let base = 'input-reset ba b--black-20 pa2 mb2 db';
+    let inputWidth = get(this, 'inputWidth');
+
+    if (inputWidth) {
+      base += ` w-${inputWidth}`;
+    }
+    return base;
+  }),
+
   placeholder: computed('label', function(){
     return `Please enter ${get(this, 'label').toLowerCase()}`
   })
